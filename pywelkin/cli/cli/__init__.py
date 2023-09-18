816fcae869f313aacdf1f5c47cb5f1c63fc2f44e
Oscar Bender-Stone <oscarbender-stone@Oscars-Laptop.local>
2023-09-17 19:07:42 -0600

    Created subpackages and licensing information

---
+++ pywelkin/cli/cli/__init__.py
@@ -0,0 +1,2 @@
+# SPDX-FileCopyrightText: 2023 Oscar Bender-Stone <oscarbenderstone@gmail.com>
+# SPDX-License-Identifier: MIT
---
+++ pywelkin/desktop/desktop/__init__.py
@@ -0,0 +1,2 @@
+# SPDX-FileCopyrightText: 2023 Oscar Bender-Stone <oscarbenderstone@gmail.com>
+# SPDX-License-Identifier: LGPL-2.1-or-later
