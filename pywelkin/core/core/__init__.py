26f35db687f049b9795de888d62ba3cb631c21a5
Oscar Bender-Stone <oscarbenderstone@gmail.com>
2023-10-09 20:11:14 -0600

    Added unstaged files from initial core work

--- pywelkin/core/core/__init__.py
+++ pywelkin/core/core/__init__.py
@@ -1,2 +1,6 @@
 # SPDX-FileCopyrightText: 2023 Oscar Bender-Stone <oscarbenderstone@gmail.com>
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: Apache-2.0-WITH-LLVM-exception
+
+from core.parser import minimal_parse
+
+minimal_parse("{ A }")
