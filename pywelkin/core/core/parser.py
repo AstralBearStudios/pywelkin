3a76caa3f6e846ff09bc360bed96eab562661191
Oscar Bender-Stone <oscarbenderstone@gmail.com>
2023-10-10 20:08:49 -0600

    Added initial boostrap and bullet grammars

--- pywelkin/core/core/parser.py
+++ pywelkin/core/core/parser.py
@@ -1,12 +1,12 @@
 # SPDX-FileCopyrightText: 2023 Oscar Bender-Stone <oscarbenderstone@gmail.com>
 # SPDX-License-Identifier: Apache-2.0-WITH-LLVM-exception
-# from lark import Lark
 import lark
 
 
+# TODO: decide how to implement regex parsing for minimal grammar
 # TODO: make the path below applicable for any OS
 def minimal_parse(input):
-    """Parse"""
+    """Parses a file with the minimal Welkin grammar"""
 
     minimal_path = "./grammars/minimal.lark"
 
@@ -15,3 +15,9 @@ def minimal_parse(input):
     output = parser.parse(input)
 
     print(output.pretty())
+
+
+# TODO: decide whether grammasr should be written in lark OR welkin config files
+def bootstrap_parse():
+    """Allows any (well-formed) Welkin config file to be a grammar."""
+    raise NotImplementedError
